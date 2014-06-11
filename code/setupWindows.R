5942d0f0a93d9b9fb30c6e822a852a1555a49db4
cmclen <chris.mclennon@gmail.com>
2014-06-11 16:53:58 -0500

    Initial commit
    Initial commit and first submission

---
+++ code/setupWindows.R
@@ -0,0 +1,15 @@
+# R Script to properly setup R Environment
+
+setupWindows <- function()
+{
+    # Set up working directory
+    setwd("~/Github/Kaggle-RandomActsOfPizza");
+    
+    # Grab data and make available as global variables
+    library(jsonlite); # Data files are JSON
+    dTest <<- fromJSON("./data/test.json");
+    dTrain <<- fromJSON("./data/train.json");
+    
+    # Write results command. Commented but left here for reference.
+    # write.csv(submit, file = "./submit/submit1.csv", row.names = FALSE);
+}
