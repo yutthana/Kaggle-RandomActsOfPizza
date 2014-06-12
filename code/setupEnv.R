13f5b775b1e3cd7d758dcbf5e7a30f11471b8fa8
cmclen <chris.mclennon@gmail.com>
2014-06-12 13:50:27 -0500

    Karma analysis

--- code/setupEnv.R
+++ code/setupEnv.R
@@ -6,10 +6,17 @@ setupEnv <- function()
     setwd("~/Github/Kaggle-RandomActsOfPizza"); # Windows
     # Mac directory goes here eventually
     
-    # Grab data and make available as global variables
+    # Grab data 
     library(jsonlite); # Data files are JSON
-    dTest <<- fromJSON("./data/test.json");
-    dTrain <<- fromJSON("./data/train.json");
+    dTest <- fromJSON("./data/test.json");
+    dTrain <- fromJSON("./data/train.json");
+    
+    # Do some basic mods to the data
+    dTrain$PostKarma <- dTrain$number_of_upvotes_of_request_at_retrieval - dTrain$number_of_downvotes_of_request_at_retrieval; # Add karma score
+    
+    # Make data available as global variables
+    dTest <<- dTest;
+    dTrain <<- dTrain;
     
     # Write results command. Commented but left here for reference.
     # write.csv(submit, file = "./submit/submit1.csv", row.names = FALSE);
